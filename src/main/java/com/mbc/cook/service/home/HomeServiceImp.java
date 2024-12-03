de72761f8ad501aabde04fd91f93931bfb3cf892
hyerin lee <helena1005@naver.com>
2024-12-03 15:01:14 +0900

    lhr: 메인페이지 개수제한

--- src/main/java/com/mbc/cook/service/home/HomeServiceImp.java
+++ src/main/java/com/mbc/cook/service/home/HomeServiceImp.java
@@ -3,6 +3,7 @@ package com.mbc.cook.service.home;
 import com.mbc.cook.entity.community.CommunityEntity;
 import com.mbc.cook.repository.home.HomeCommuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -33,6 +34,6 @@ public class HomeServiceImp implements HomeService {
 
     @Override
     public List<CommunityEntity> findAll() {
-        return homeRepository.findAll();
+        return homeRepository.findAll(Sort.by(Sort.Direction.DESC, "communityNum"));
     }
 }
