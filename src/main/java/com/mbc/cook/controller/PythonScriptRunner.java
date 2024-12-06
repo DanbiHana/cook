af162bf544eb8c72159fa7787ce961b1b559c4f8
leehyell <helena1005@naver.com>
2024-12-07 00:48:56 +0900

    create 모드일 때 에러 문자 세분화

--- src/main/java/com/mbc/cook/controller/PythonScriptRunner.java
+++ src/main/java/com/mbc/cook/controller/PythonScriptRunner.java
@@ -3,6 +3,7 @@ package com.mbc.cook.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -12,22 +13,31 @@ public class PythonScriptRunner implements CommandLineRunner {
     private String ddlAuto;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         if ("create".equalsIgnoreCase(ddlAuto)) {
-            // Python 스크립트 실행 로직 (위 코드와 동일)
-            ProcessBuilder processBuilder = new ProcessBuilder("python", "C:\\project\\cook\\src\\main\\resources\\templates\\sql\\first_insert.py");
-            Process process = processBuilder.start();
+            try {
+                System.out.println("DDL-AUTO 값이 'create'입니다. Python 스크립트를 실행합니다.");
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                ProcessBuilder processBuilder = new ProcessBuilder("python",
+                        "C:\\project\\cook\\src\\main\\resources\\templates\\sql\\first_insert.py");
+                processBuilder.redirectErrorStream(true);
+                Process process = processBuilder.start();
+
+                int exitCode = process.waitFor();
+                System.out.println("Python 스크립트 종료 코드: " + exitCode);
+
+                if (exitCode == 0) {
+                    System.out.println("Python 스크립트가 정상적으로 실행되었습니다.");
+                } else {
+                    System.err.println("Python 스크립트 실행 중 문제가 발생했습니다.");
+                }
+
+            } catch (Exception e) {
+                System.err.println("Python 스크립트를 실행하는 동안 예외가 발생했습니다: " + e.getMessage());
+                e.printStackTrace();
             }
-
-            int exitCode = process.waitFor();
-            System.out.println("Python 스크립트 종료 코드: " + exitCode);
-            System.out.println("종료 코드 0: 문제 없음");
-            System.out.println("종료 코드 1: 뭔가 문제 있음");
+        } else {
+            System.out.println("DDL-AUTO 값이 'create'가 아니므로 Python 스크립트를 실행하지 않습니다.");
         }
     }
 }
